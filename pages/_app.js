e9af3f8ec3e4045153d6262ac2a559be51cb3565
Santiago Guastavino <san.guasta@gmail.com>
2023-03-26 20:18:26 -0300

    added analytics package

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { useEffect } from 'react'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp ({ Component, pageProps }) {
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_DEFAULT_URL)
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
